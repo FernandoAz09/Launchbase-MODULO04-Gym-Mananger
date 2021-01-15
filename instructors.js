b16fafde60113f134d793c3f1412a6198ec8985f
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-01-15 15:22:17 -0300

    Criando novas rotas + data.json

--- instructors.js
+++ instructors.js
@@ -1,3 +1,22 @@
+const fs = require('fs') // módulo do Node.js **FILE SYSTEM trabalha com arquivos do sistema
+const data = require('./data.json')
+
+// req.query -> recebendo pela instrução da rota -> ?id=1
+// req.body -> recebendo a partir do corpo da requisição(form)
+// req.params-> recebendo pela instrução da rota -> /:id/:
+
+//show
+exports.show = function(req,res) {
+    const { id } = req.params
+
+    const foudInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foudInstructor) return res.send("Instructor not found!")
+
+    return res.send(foudInstructor)
+}
 //create
 exports.post = function(req, res) { //exportando apenas as functions
 
@@ -9,8 +28,39 @@ exports.post = function(req, res) { //exportando apenas as functions
             return res.send('Por favor, preencha todos os campos.') // envie essa mensagem
         }
     }
+
+    /* --------------------- Escrevendo no data.json --------------------- */
+    let {avatar_url, name, birth, gender, services} = req.body //usando o LET pois poderão ser alteradas
+    
+    birth = Date.parse(birth) //Transformando e escrevendo as datas de nasc. em milissegundos 
+    const created_at = Date.now() // Adicionando e escrevendo a data de criação em milissegundos  
+    const id = Number(data.instructors.length + 1) // Adicionando um ID numeral crescente ao instrutor
+
+    /* ---------------------  Desestruturando o req.body  ---------------------*/
+
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    }) 
+    
+    /* --------------------- TRANSFORMANDO OS DADOS DIGITADOS EM JSON ---------------------*/
+
+//Módulo.função(CAMINHO(path), CONSTRUCTOR JSON.método conversor(onde passará os valores, null, 2 espaços).MÉTODO(VALOR), FUNCTION(CALLBACK))
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){ 
+        if (err) return res.send("Write file error.") 
+
+        return res.redirect("/instructors")
+    })
+
     // corpo da requisição POST por isso req.body
-    return res.send(req.body)
+    // return res.send(req.body)
 }
 
 
--- routes.js
+++ routes.js
@@ -11,9 +11,11 @@ routes.get('/instructors', function(req, res) {
     return res.render("instructors/index") // renderizando a página inicial
 })
 
-routes.get('/instructors/create', function(rec, res) {
+routes.get('/instructors/create', function(rec, res) { // Rota de criação
     return res.render("instructors/create")
 })
+
+routes.get("/instructors/:id", instructors.show) //recebendo o id dentro do express
 
 routes.post("/instructors", instructors.post) //passando a function de outro arquivo e trazendo para o routes
 
