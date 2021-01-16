8b15f2082dbc9db54540b46c10f907366b145013
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-01-16 12:27:34 -0300

    Update ERRO

--- instructors.js
+++ instructors.js
@@ -1,23 +1,32 @@
-const fs = require('fs') // módulo do Node.js **FILE SYSTEM trabalha com arquivos do sistema
+const fs = require('fs') // Módulo do Node.js **FILE SYSTEM trabalha com arquivos do sistema
 const data = require('./data.json')
 
 // req.query -> recebendo pela instrução da rota -> ?id=1
 // req.body -> recebendo a partir do corpo da requisição(form)
 // req.params-> recebendo pela instrução da rota -> /:id/:
 
-//show
-exports.show = function(req,res) {
-    const { id } = req.params
+////////////////////////        SHOW
+exports.show = function(req,res) { 
+    const { id } = req.params // desestruturando, retirando o ID do req.params
 
-    const foudInstructor = data.instructors.find(function(instructor){
-        return instructor.id == id
+    const foudInstructor = data.instructors.find(function(instructor){ // Encontrando(com o find) o ID dentro de instructor
+        return instructor.id == id // FIND -> BOOLEAN
     })
 
     if (!foudInstructor) return res.send("Instructor not found!")
 
-    return res.send(foudInstructor)
+    const instructor = { // Configurando os dados apresentados
+        ...foudInstructor, // Os ajustes abaixo serão feitos e o restante ficara espalhado dentro do foundInstructors
+        age: "",
+        gender: "",
+        services: foudInstructor.services.split(","),
+        created_at: "",
+    }
+
+    return res.render("instructors/show", { instructor: foudInstructor}) // Renderizando os dados na página show.njk
 }
-//create
+
+////////////////////////        CREATE
 exports.post = function(req, res) { //exportando apenas as functions
 
     /* ------------------------------ ESTRUTURA DE VALIDAÇÃO ------------------------------*/
@@ -25,19 +34,24 @@ exports.post = function(req, res) { //exportando apenas as functions
 
     for(key of keys) { //Verificando se algum campo(key) está vazio
         if(req.body[key] == "") { // Se algum campo estiver vazio... 
-            return res.send('Por favor, preencha todos os campos.') // envie essa mensagem
+            return res.send('Por favor, preencha todos os campos.') // Envie essa mensagem
         }
     }
 
-    /* --------------------- Escrevendo no data.json --------------------- */
-    let {avatar_url, name, birth, gender, services} = req.body //usando o LET pois poderão ser alteradas
+
+    /* ---------------------  Desestruturando o req.body  ---------------------*/
     
+    let {avatar_url, name, birth, gender, services} = req.body // Usando o LET pois poderão ser alteradas
+    
+
+    /* --------------------- TRATAMENTO DOS DADOS --------------------- */
+
     birth = Date.parse(birth) //Transformando e escrevendo as datas de nasc. em milissegundos 
     const created_at = Date.now() // Adicionando e escrevendo a data de criação em milissegundos  
     const id = Number(data.instructors.length + 1) // Adicionando um ID numeral crescente ao instrutor
+    
 
-    /* ---------------------  Desestruturando o req.body  ---------------------*/
-
+    /* --------------------- Escrevendo no data.json --------------------- */
 
     data.instructors.push({
         id,
@@ -49,9 +63,10 @@ exports.post = function(req, res) { //exportando apenas as functions
         created_at
     }) 
     
+
     /* --------------------- TRANSFORMANDO OS DADOS DIGITADOS EM JSON ---------------------*/
 
-//Módulo.função(CAMINHO(path), CONSTRUCTOR JSON.método conversor(onde passará os valores, null, 2 espaços).MÉTODO(VALOR), FUNCTION(CALLBACK))
+// Módulo.função(CAMINHO(path), CONSTRUCTOR JSON.método conversor(onde passará os valores, null, 2 espaços).MÉTODO(VALOR), FUNCTION(CALLBACK))
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){ 
         if (err) return res.send("Write file error.") 
@@ -59,11 +74,11 @@ exports.post = function(req, res) { //exportando apenas as functions
         return res.redirect("/instructors")
     })
 
-    // corpo da requisição POST por isso req.body
+    // Corpo da requisição POST por isso req.body
     // return res.send(req.body)
 }
 
 
-//update
+////////////////////////        UPDATE
 
-//delete
+////////////////////////        DELETE
